8ba0938af7194ca58a88a735dc35c7cae5afc986
张明瑞 <z1m6r3@gmail.com>
2016-03-07 10:46:16 +0800

    hw4

--- hw4/mall.c
+++ hw4/mall.c
@@ -1,87 +1,134 @@
-#include <stdlib.h>
-#include <assert.h>
-#include <stdio.h>
-#include <string.h>
+/* Include the sbrk function */
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define NALLOC 1024        // minimum #units to request
-struct Header { struct Header *ptr; uint size; };
+int has_initialized = 0;
+void *managed_memory_start;
+void *last_valid_address;
 
-static Header base;                       // empty list to get started
-static Header *freep = NULL;              // start of free list
+struct mem_control_block {
+    int is_available;
+    int size;
+};
 
-struct {                      // block header
-    union header *ptr;        // next block if on free list
-    unsigned size;            // size of this block
-} s;
-
-void *_malloc(size_t nbytes) {
-    Header *p, *prevp;
-    unsigned nunits = (nbytes+sizeof(Header)-1)/sizeof(Header) + 1;
-
-    if ((prevp = freep) == NULL) {
-        base.s.ptr = freep = prevp = &base;
-        base.s.size = 0;
-    }
-
-    for (p = prevp->s.ptr ;; prevp = p, p = p->s.ptr) {
-        if (p->size >= nunits) {          // big enough
-            if (p->s.size == nunits)
-                prevp->s.ptr = p->s.ptr;
-            else {                        // allocate tail end
-                p->size -= nunits;
-                p += p->s.size;
-                p->s.size = nunits;
-            }
-            freep = prevp;
-            return (void*)(p+1);
-        }
-
-        if (p == freep)                  // wrapped around free list
-            if ((p = morecore(nunits)) == NULL)
-                return NULL;
-    }
-}
-
-static Header *morecore(unsigned nu){
-    char *cp;
-    Header *up;
-
-    if (nu < NALLOC)
-        nu = NALLOC;
-    cp = sbrk(nu * sizeof(Header));
-    if (cp == (char*)-1)   // check
-        return NULL;
-    up = (Header*) cp;
-    up->s.size = nu;
-    free((void*)(up+1));
-    return freep;
-}
-
-void _free(void *ap)
+void malloc_init()
 {
-    Header *bp, *p;
-    bp = (Header *)ap - 1; /* point to block header */
-    for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
-        if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
-            break; /* freed block at start or end of arena */
-
-    if (bp + bp->size == p->s.ptr) { /* join to upper nbr */
-        bp->s.size += p->s.ptr->s.size;
-        bp->s.ptr = p->s.ptr->s.ptr;
-    } else
-        bp->s.ptr = p->s.ptr;
-        if (p + p->size == bp) { /* join to lower nbr */
-            p->s.size += bp->s.size;
-            p->s.ptr = bp->s.ptr;
-        } else
-            p->s.ptr = bp;
-        freep = p;
+    /* grab the last valid address from the OS */
+    last_valid_address = sbrk(0);
+    /* we don't have any memory to manage yet, so
+     *just set the beginning to be last_valid_address
+     */
+    managed_memory_start = last_valid_address;
+    /* Okay, we're initialized and ready to go */
+    has_initialized = 1;
 }
+
+void free(void *firstbyte) {
+    struct mem_control_block *mcb;
+    /* Backup from the given pointer to find the
+     * mem_control_block
+     */
+    mcb = (struct mem_control_block *)((char*)firstbyte - sizeof(struct mem_control_block));
+    /* Mark the block as being available */
+    mcb->is_available = 1;
+    /* That's It!  We're done. */
+    return;
+}
+
+void *malloc(long numbytes) {
+    /* Holds where we are looking in memory */
+    void *current_location;
+    /* This is the same as current_location, but cast to a
+     * memory_control_block
+     */
+    struct mem_control_block *current_location_mcb;
+    /* This is the memory location we will return.  It will
+     * be set to 0 until we find something suitable
+     */
+    void *memory_location;
+    /* Initialize if we haven't already done so */
+    if(! has_initialized)   {
+        malloc_init();
+    }
+    /* The memory we search for has to include the memory
+     * control block, but the users of malloc don't need
+     * to know this, so we'll just add it in for them.
+     */
+    numbytes = numbytes + sizeof(struct mem_control_block);
+    /* Set memory_location to 0 until we find a suitable
+     * location
+     */
+    memory_location = 0;
+    /* Begin searching at the start of managed memory */
+    current_location = managed_memory_start;
+    /* Keep going until we have searched all allocated space */
+    while(current_location != last_valid_address)
+    {
+        /* current_location and current_location_mcb point
+         * to the same address.  However, current_location_mcb
+         * is of the correct type, so we can use it as a struct.
+         * current_location is a void pointer so we can use it
+         * to calculate addresses.
+         */
+        current_location_mcb =
+            (struct mem_control_block *)current_location;
+        if(current_location_mcb->is_available)
+        {
+            if(current_location_mcb->size >= numbytes)
+            {
+                /* Woohoo!  We've found an open,
+                 * appropriately-size location.
+                 */
+                /* It is no longer available */
+                current_location_mcb->is_available = 0;
+                /* We own it */
+                memory_location = current_location;
+                /* Leave the loop */
+                break;
+            }
+        }
+        /* If we made it here, it's because the Current memory
+         * block not suitable; move to the next one
+         */
+        current_location = (char*)current_location + current_location_mcb->size;
+    }
+    /* If we still don't have a valid location, we'll
+     * have to ask the operating system for more memory
+     */
+    if(! memory_location)
+    {
+        /* Move the program break numbytes further */
+        sbrk(numbytes);
+        /* The new memory will be where the last valid
+         * address left off
+         */
+        memory_location = last_valid_address;
+        /* We'll move the last valid address forward
+         * numbytes
+         */
+        last_valid_address = (char*)last_valid_address + numbytes;
+        /* We need to initialize the mem_control_block */
+        current_location_mcb = (struct mem_control_block *)memory_location;
+        current_location_mcb->is_available = 0;
+        current_location_mcb->size = numbytes;
+    }
+    /* Now, no matter what (well, except for error conditions),
+     * memory_location has the address of the memory, including
+     * the mem_control_block
+     */
+    /* Move the pointer past the mem_control_block */
+    memory_location = (char*)memory_location + sizeof(struct mem_control_block);
+    /* Return the pointer */
+    return memory_location;
+ }
 
 int main(){
-    int *a = _malloc(sizeof(int));
-    _free(a);
-    printf("(haha!)\n");
+    malloc_init();
+    int * num = (int*)malloc(sizeof(int));
+    int * arrs = (int*)malloc(sizeof(int)*10);
+    free(num);
+    free(arrs);
+    printf("finished.\n");
     return 0;
 }
